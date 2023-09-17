3c0b00eff5d4e7666e1dfd91e7f0ad0d4a44138e
Thien Duc <ntduc4@gmail.com>
2023-09-17 23:07:20 +0800

    Redesign createPost page

--- src/components/createPost.tsx
+++ src/components/createPost.tsx
@@ -1,81 +1,75 @@
-import Markdown from "markdown-to-jsx";
-import React, { useState } from "react";
+import { useState } from "react";
 import createPost from "./createPost.module.css"
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 
 const CreatePost = () => {
     let [post, setPost] = useState("")
+    let [isPublic, setIsPublic] = useState(true)
+    let [title, setTitle] = useState("")
     let [description, setDescription] = useState("")
+    let [tags, setTags] = useState([] as string[])
+    const tagChange = (value: string) => {
+        let tagsCopy = [...tags]
+        if (tagsCopy.includes(value)) {
+            tagsCopy.splice(tagsCopy.indexOf(value), 1)
+            setTags(tagsCopy)
+        } else {
+            setTags([...tags, value])
+        }
+    }
+
     return (
-        <>
-            <h1
-                className={createPost.center}
-                style={{
-                    marginTop: "5vh"
-                }}
-            >Create your post</h1>
-
-            <input
-                type="text"
-                id="title"
-                placeholder="Enter your post title"
-                className={`${createPost.center} ${createPost.prettyTitle} ${createPost.inputfocus}`}
-                style={{
-                    marginBottom: 30,
-                    marginTop: 30,
-                }}
-            />
-
-            <textarea
-                id="description"
-                placeholder="Description of the post"
-                style={{
-                    margin: "30px auto 30px auto"
-                }}
-                value={description}
-                onChange={(e) => { setDescription(e.target.value) }}
-                className={`${createPost.center} ${createPost.prettyDescription} ${createPost.inputfocus}`}
-            />
-
-            <textarea
-                id="post"
-                placeholder="Type out your thought"
-                style={{
-                    marginTop: 30
-                }}
-                value={post}
-                onChange={(e) => { setPost(e.target.value) }}
-                className={`${createPost.center} ${createPost.prettyPost} ${createPost.inputfocus}`}
-            />
-
-
-
-            <div className={createPost.center} style={{ margin: "30px auto 30px auto" }}>
-                <input
-                    type="text"
-                    id="tags"
-                    placeholder="tags"
-                    className={`${createPost.prettyTags}`}
-                />
+        <div className={createPost.site}>
+            <div id="mainContent" className={createPost.mainContent} >
+                <input type="text" placeholder="Title" className={createPost.title} />
+                <textarea placeholder="Description" className={createPost.description} />
+                <ReactQuill value={post} onChange={setPost} />
             </div>
 
+            <div id="options" className={createPost.options}>
+                <div id="status" className={createPost.status}>
+                    <h3>Publish</h3>
+                    <span>Status: Draft</span>
+                    <span>Visiblity: {isPublic ? "Public" : "Private"}</span>
+                    <button>Save as draft</button>
+                    <button>Publish</button>
+                </div>
+                <div id="tags" className={createPost.tags}>
+                    <span>
+                        <input type="checkbox" value={"anime"} name="anime" onChange={() => { tagChange("anime") }} />
+                        <label htmlFor="anime">anime</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" value={"review"} name="review" onChange={() => { tagChange("review") }} />
+                        <label htmlFor="review">review</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" value={"rant"} name="rant" onChange={() => { tagChange("rant") }} />
+                        <label htmlFor="rant">rant</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" value={"theory"} name="theory" onChange={() => { tagChange("theory") }} />
+                        <label htmlFor="theory">theory</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" value={"advice"} name="advice" onChange={() => { tagChange("advice") }} />
+                        <label htmlFor="advice">advice</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" value={"modReview"} name="modReview" onChange={() => { tagChange("modReview") }} />
+                        <label htmlFor="modReview">modReview</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" value={"tutorial"} name="tutorial" onChange={() => { tagChange("tutorial") }} />
+                        <label htmlFor="tutorial">tutorial</label>
+                    </span>
+                    <span>{tags}</span>
 
-            <h1
-                className={createPost.center}
-                style={{
-                    marginTop: 50,
-                    marginBottom: 20
-                }}
-            >Preview</h1>
-            <Markdown
-                options={{ forceBlock: true }}
-                style={{ "white-space": "pre-wrap" }}
-                className={createPost.center}
-            >
-                {post}
-            </Markdown>
+                </div>
 
-
-        </>
+            </div>
+        </div>
     )
 }
 
