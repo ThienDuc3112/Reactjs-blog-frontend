47e927018d89132d2458495679c094162d15d3d1
Thien Duc <ntduc4@gmail.com>
2023-09-17 23:07:19 +0800

    Some progress on createPost design

--- src/components/createPost.tsx
+++ src/components/createPost.tsx
@@ -4,28 +4,61 @@ import createPost from "./createPost.module.css"
 
 const CreatePost = () => {
     let [post, setPost] = useState("")
+    let [description, setDescription] = useState("")
     return (
         <>
-            <h1 className={createPost.center}>Create your post</h1>
-            <input
-                type="text"
-                placeholder="Enter your post title"
+            <h1
                 className={createPost.center}
                 style={{
-                    marginBottom: 20,
-                    marginTop: 20,
+                    marginTop: "5vh"
+                }}
+            >Create your post</h1>
+
+            <input
+                type="text"
+                id="title"
+                placeholder="Enter your post title"
+                className={`${createPost.center} ${createPost.prettyTitle} ${createPost.inputfocus}`}
+                style={{
+                    marginBottom: 30,
+                    marginTop: 30,
                 }}
             />
+
             <textarea
+                id="description"
+                placeholder="Description of the post"
+                style={{
+                    margin: "30px auto 30px auto"
+                }}
+                value={description}
+                onChange={(e) => { setDescription(e.target.value) }}
+                className={`${createPost.center} ${createPost.prettyDescription} ${createPost.inputfocus}`}
+            />
+
+            <textarea
+                id="post"
                 placeholder="Type out your thought"
                 style={{
-                    marginTop: 20
+                    marginTop: 30
                 }}
                 value={post}
                 onChange={(e) => { setPost(e.target.value) }}
-                className={`${createPost.center} ${createPost.prettyTextArea}`}
-
+                className={`${createPost.center} ${createPost.prettyPost} ${createPost.inputfocus}`}
             />
+
+
+
+            <div className={createPost.center} style={{ margin: "30px auto 30px auto" }}>
+                <input
+                    type="text"
+                    id="tags"
+                    placeholder="tags"
+                    className={`${createPost.prettyTags}`}
+                />
+            </div>
+
+
             <h1
                 className={createPost.center}
                 style={{
